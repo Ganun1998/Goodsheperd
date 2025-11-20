35eb1b1c140035e75d449de9ccfc2891c85309dd
Ganun1998 <ganungattang1@gmail.com>
2025-11-20 08:28:25 +0300

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Good-sheperd",
+  base: process.env.VITE_BASE_PATH || "/Goodsheperd",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
